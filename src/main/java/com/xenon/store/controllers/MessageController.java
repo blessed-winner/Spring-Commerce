b1efd960f7542a6b82210a838383bdce6a4c1d3d
Winner <blessedwinner66@gmail.com>
2026-02-02 21:17:27 +0200

    Addition of message entity

--- src/main/java/com/xenon/store/controllers/MessageController.java
+++ src/main/java/com/xenon/store/controllers/MessageController.java
@@ -1,12 +1,13 @@
 package com.xenon.store.controllers;
 
+import com.xenon.store.entities.Message;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
     @RequestMapping("/hello")
-    public String sayHello(){
-         return "Hello World";
+    public Message sayHello(){
+         return new Message("Hello World");
     }
 }
