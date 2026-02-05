e3b57fe487cb39ae9738c9bb6787a64f2c10edd7
Winner <blessedwinner66@gmail.com>
2026-02-05 15:14:58 +0200

    Solved the n+1 problem associated with product endpoints

--- src/main/java/com/xenon/store/repositories/ProductRepository.java
+++ src/main/java/com/xenon/store/repositories/ProductRepository.java
@@ -1,10 +1,17 @@
 package com.xenon.store.repositories;
 
 import com.xenon.store.entities.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @EntityGraph(attributePaths = "category")
     List<Product>findByCategory_Id(Byte categoryId);
+
+    @EntityGraph(attributePaths = "category")
+    @Query("SELECT p FROM Product p")
+    List<Product>findAllWithCategory();
 }
