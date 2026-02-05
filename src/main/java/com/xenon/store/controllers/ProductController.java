7c7443916698cb9fefdd84a40942a869af26b1bb
Winner <blessedwinner66@gmail.com>
2026-02-05 10:03:50 +0200

    Get product by id functionality

--- src/main/java/com/xenon/store/controllers/ProductController.java
+++ src/main/java/com/xenon/store/controllers/ProductController.java
@@ -1,10 +1,14 @@
 package com.xenon.store.controllers;
 
 import com.xenon.store.dto.ProductDto;
+import com.xenon.store.entities.Product;
 import com.xenon.store.mappers.ProductMapper;
 import com.xenon.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +25,14 @@ public class ProductController {
                                 .stream()
                                 .map(productMapper::toDto)
                                 .toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> getProduct(@PathVariable Long id){
+        var product = productRepository.findById(id).orElse(null);
+        if(product == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(productMapper.toDto(product));
     }
 }
