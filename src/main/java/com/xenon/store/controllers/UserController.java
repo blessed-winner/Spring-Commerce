a2aaea004ba5b11d9fa398292ef55bc402281c30
Winner <blessedwinner66@gmail.com>
2026-02-06 14:56:24 +0200

    Action-based updates

--- src/main/java/com/xenon/store/controllers/UserController.java
+++ src/main/java/com/xenon/store/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.xenon.store.controllers;
 
+import com.xenon.store.dto.ChangePasswordRequest;
 import com.xenon.store.dto.RegisterUserRequest;
 import com.xenon.store.dto.UpdateUserRequest;
 import com.xenon.store.dto.UserDto;
@@ -8,6 +9,7 @@ import com.xenon.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -78,6 +80,26 @@ public class UserController {
         }
 
         userRepository.delete(user);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{id}/change-password")
+    public ResponseEntity<Void>changePassword(
+            @PathVariable Long id,
+            @RequestBody ChangePasswordRequest request
+            ){
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        if(!user.getPassword().equals(request.getOldPassword())){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        user.setPassword(request.getNewPassword());
+        userRepository.save(user);
+
         return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/com/xenon/store/dto/ChangePasswordRequest.java
@@ -0,0 +1,9 @@
+package com.xenon.store.dto;
+
+import lombok.Data;
+
+@Data
+public class ChangePasswordRequest {
+    private String oldPassword;
+    private String newPassword;
+}
