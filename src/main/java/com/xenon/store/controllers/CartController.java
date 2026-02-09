5590e0c603323e2c95620a824e1acc722434c348
Winner <blessedwinner66@gmail.com>
2026-02-09 14:39:55 +0200

    Finalization of create cart controller

--- src/main/java/com/xenon/store/controllers/CartController.java
+++ src/main/java/com/xenon/store/controllers/CartController.java
@@ -1,6 +1,8 @@
 package com.xenon.store.controllers;
 
 import com.xenon.store.dto.CartDto;
+import com.xenon.store.mappers.CartMapper;
+import com.xenon.store.repositories.CartRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,12 +11,21 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController("/carts")
 public class CartController {
+  private CartRepository cartRepository;
+  private CartMapper cartMapper;
 
   @PostMapping
     public ResponseEntity<CartDto> createCart(
             @RequestBody CartDto cartDto,
             UriComponentsBuilder uriBuilder
   ){
+     var cart = cartMapper.toEntity(cartDto);
+     cartRepository.save(cart);
+
+     var cartCreateRequest = cartMapper.toDto(cart);
+     var uri = uriBuilder.path("/carts/{id}").buildAndExpand(cartCreateRequest.getId()).toUri();
+
+     return ResponseEntity.created(uri).body(cartCreateRequest);
 
   }
 }
