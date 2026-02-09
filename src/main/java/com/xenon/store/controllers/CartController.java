41493c94ad3826adb6d16a435afc879a3ec7e267
Winner <blessedwinner66@gmail.com>
2026-02-09 14:29:28 +0200

    Starting the create cart controller

--- src/main/java/com/xenon/store/controllers/CartController.java
+++ src/main/java/com/xenon/store/controllers/CartController.java
@@ -1,12 +1,14 @@
 package com.xenon.store.controllers;
 
+import com.xenon.store.dto.CartDto;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/carts")
 public class CartController {
   @PostMapping
-    public ResponseEntity<> createCart(){
+    public ResponseEntity<CartDto> createCart(){
 
   }
 }
