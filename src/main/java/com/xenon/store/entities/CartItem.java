b1d6f69199328bdf75c517d4d2a55a05f3dab3f3
Winner <blessedwinner66@gmail.com>
2026-02-09 11:38:02 +0200

    Defining cart and cart-item entities

---
+++ src/main/java/com/xenon/store/entities/CartItem.java
@@ -0,0 +1,17 @@
+package com.xenon.store.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "cart_item")
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+}
