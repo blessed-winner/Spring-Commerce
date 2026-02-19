f79754602aefc4815207e3d2fe2d1abb2827f8e8
Winner <blessedwinner66@gmail.com>
2026-02-19 20:29:56 +0200

    Full product addition to cart complete

--- src/main/java/com/xenon/store/dto/CartItemDto.java
+++ src/main/java/com/xenon/store/dto/CartItemDto.java
@@ -6,6 +6,7 @@ import java.math.BigDecimal;
 
 @Data
 public class CartItemDto {
+    private CartProductDto product;
     private Integer quantity;
     private BigDecimal totalPrice;
 }
--- src/main/java/com/xenon/store/mappers/CartMapper.java
+++ src/main/java/com/xenon/store/mappers/CartMapper.java
@@ -2,8 +2,10 @@ package com.xenon.store.mappers;
 
 import com.xenon.store.dto.CartDto;
 import com.xenon.store.dto.CartItemDto;
+import com.xenon.store.dto.CartProductDto;
 import com.xenon.store.entities.Cart;
 import com.xenon.store.entities.CartItem;
+import com.xenon.store.entities.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -14,4 +16,6 @@ public interface CartMapper {
 
     @Mapping(target = "totalPrice", expression = "java(cartItem.getTotalPrice())")
     CartItemDto toDto(CartItem cartItem);
+
+    CartProductDto toDto(Product product);
 }
