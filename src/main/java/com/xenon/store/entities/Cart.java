cbb9b9cc732bc4daa47c6501edfefe486982f10e
Winner <blessedwinner66@gmail.com>
2026-02-19 20:44:24 +0200

    Modifications for get cart response

--- src/main/java/com/xenon/store/entities/Cart.java
+++ src/main/java/com/xenon/store/entities/Cart.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.*;
 
@@ -23,5 +24,12 @@ public class Cart {
     private LocalDateTime createdAt;
 
     @OneToMany(mappedBy = "cart", cascade = CascadeType.MERGE)
-    private Set<CartItem> cartItems = new LinkedHashSet<>();
+    private Set<CartItem> items = new LinkedHashSet<>();
+
+    public BigDecimal getTotalPrice(){
+        return items.stream()
+                .map(CartItem::getTotalPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
 }
