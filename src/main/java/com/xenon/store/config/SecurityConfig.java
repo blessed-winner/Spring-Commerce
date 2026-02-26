3b49b33f41178e59d0ac91576d74d41c8a58696e
Winner <blessedwinner66@gmail.com>
2026-02-26 21:45:42 +0200

    Password encoding while registering a user

--- src/main/java/com/xenon/store/config/SecurityConfig.java
+++ src/main/java/com/xenon/store/config/SecurityConfig.java
@@ -2,15 +2,23 @@ package com.xenon.store.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
       //Stateless sessions(token-based auth)
@@ -19,9 +27,12 @@ public class SecurityConfig{
         http
                 .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(c -> c.anyRequest().permitAll());
+                .authorizeHttpRequests(
+                        c -> c.requestMatchers("/carts/**").permitAll()
+                                                            .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                                                            .anyRequest().authenticated()
 
-
-      return http.build();
+                );
+        return http.build();
   }
 }
