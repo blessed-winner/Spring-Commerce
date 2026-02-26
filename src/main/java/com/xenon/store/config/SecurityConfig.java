a607a43f4524046410527dcc668dc9b4e5c71eed
Winner <blessedwinner66@gmail.com>
2026-02-26 20:22:25 +0200

    Addition of security filter chain class

---
+++ src/main/java/com/xenon/store/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.xenon.store.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+      //Stateless sessions(token-based auth)
+      //Disable CSRF
+      //Authorize
+        http
+                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(c -> c.anyRequest().permitAll());
+
+
+      return http.build();
+  }
+}
