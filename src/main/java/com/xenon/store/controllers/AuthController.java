f6d1ebf9f4f7ac35f52aa47ad8798b5d9547e6e7
Winner <blessedwinner66@gmail.com>
2026-02-28 11:24:38 +0200

    Correction of error in auth controller

--- src/main/java/com/xenon/store/controllers/AuthController.java
+++ src/main/java/com/xenon/store/controllers/AuthController.java
@@ -7,6 +7,8 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,20 +21,18 @@ import java.util.Optional;
 @RequestMapping("/auth")
 @AllArgsConstructor
 public class AuthController {
-    private UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/login")
   public ResponseEntity<Void> login(
           @Valid @RequestBody LoginRequest request
   ){
-     var user = userRepository.findByEmail(request.getEmail()).orElse(null);
-     if(user == null){
-         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-     }
-     if(!passwordEncoder.matches(request.getPassword(), user.getPassword())){
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-     }
+    authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                    request.getEmail(),
+                    request.getPassword()
+            )
+    );
 
      return ResponseEntity.ok().build();
   }
