0060e12ae3c410a7317ef9c34cf019d3cc6ced81
Winner <blessedwinner66@gmail.com>
2026-03-02 08:52:13 +0200

    Beginning of authentication custom filter

---
+++ src/main/java/com/xenon/store/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,39 @@
+package com.xenon.store.filters;
+
+import com.xenon.store.services.JwtService;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@AllArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        var token = authHeader.replace("Bearer ", "");
+        if(!jwtService.validateToken(token)){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        var authentication = new UsernamePasswordAuthenticationToken(
+                jwtService.getEmailFromToken(token),
+                null,
+                null
+        );
+    }
+}
