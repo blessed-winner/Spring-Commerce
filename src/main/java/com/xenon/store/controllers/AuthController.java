c4f6e72269a702e2fa0bcd8094a549a89e5dec3d
Winner <blessedwinner66@gmail.com>
2026-03-03 16:55:27 +0200

    Refresh endpoint for token refresh

--- src/main/java/com/xenon/store/controllers/AuthController.java
+++ src/main/java/com/xenon/store/controllers/AuthController.java
@@ -57,11 +57,19 @@ public ResponseEntity<JwtResponse> login(
     return ResponseEntity.ok(new JwtResponse(accessToken));
   }
 
-   @PostMapping("/validate")
-    public boolean validate(@RequestHeader("Authorization") String authHeader){
+    @PostMapping("/refresh")
+    public ResponseEntity<JwtResponse> refresh(
+            @CookieValue(value = "refreshToken") String refreshToken
+    ){
+        if(!jwtService.validateToken(refreshToken)){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
 
-        var token = authHeader.replace("Bearer ", "");
-        return jwtService.validateToken(token);
+        var userId = jwtService.getUserIdFromToken(refreshToken);
+        var user = userRepository.findById(userId).orElseThrow();
+        var accessToken = jwtService.generateAccessToken(user);
+
+         return ResponseEntity.ok(new JwtResponse(accessToken));
     }
 
     @GetMapping("/me")
