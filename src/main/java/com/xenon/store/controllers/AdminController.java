60c88c39ccecfb3c0dcd78e00e2eca18d6f02852
Winner <blessedwinner66@gmail.com>
2026-04-01 19:45:26 +0200

    Removed unnecessary comments

--- src/main/java/com/xenon/store/controllers/AdminController.java
+++ src/main/java/com/xenon/store/controllers/AdminController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//Admin only
 @RequestMapping("/admin")
 public class AdminController {
     @GetMapping("/hello")
